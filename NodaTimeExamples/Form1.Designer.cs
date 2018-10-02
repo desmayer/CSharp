9cf7798995accf8654837fd1de1a4a86bdd7b0bc
Desmond Mayer <desmond.mayer@intertek.com>
2018-10-02 09:54:40 +0100

    Add project files.

---
+++ NodaTimeExamples/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace NodaTimeExamples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtDateTimeNow = new System.Windows.Forms.TextBox();
+            this.txtInstant = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(12, 44);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(527, 20);
+            this.txtDate.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 70);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 96);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 123);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(527, 78);
+            this.textBox1.TabIndex = 3;
+            // 
+            // txtDateTimeNow
+            // 
+            this.txtDateTimeNow.Location = new System.Drawing.Point(12, 12);
+            this.txtDateTimeNow.Name = "txtDateTimeNow";
+            this.txtDateTimeNow.Size = new System.Drawing.Size(527, 20);
+            this.txtDateTimeNow.TabIndex = 4;
+            // 
+            // txtInstant
+            // 
+            this.txtInstant.Location = new System.Drawing.Point(12, 207);
+            this.txtInstant.Multiline = true;
+            this.txtInstant.Name = "txtInstant";
+            this.txtInstant.Size = new System.Drawing.Size(527, 179);
+            this.txtInstant.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInstant);
+            this.Controls.Add(this.txtDateTimeNow);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtDate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtDateTimeNow;
+        private System.Windows.Forms.TextBox txtInstant;
+    }
+}
+
---
+++ NodaTimeExamples/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NodaTime;
+using NodaTime.Text;
+using NodaTime.TimeZones;
+
+namespace NodaTimeExamples
+{
+    public partial class Form1 : Form
+    {
+        public DateTimeZone datetimeZone;
+        public CultureInfo culture;
+        public CultureInfo defaultCulture = new CultureInfo("en-GB");
+        public Form1()
+        {
+            ReadOnlyCollection<TimeZoneInfo> timezones;
+            timezones = TimeZoneInfo.GetSystemTimeZones();
+            InitializeComponent();
+            Instant i2 = Instant.FromDateTimeOffset(DateTime.Now);
+            Instant now = SystemClock.Instance.GetCurrentInstant();
+            DateTimeZone tz = DateTimeZoneProviders.Tzdb["Europe/London"];
+            ZonedDateTime zdt = now.InZone(tz);
+            //This will be the best way to display a datetime?
+            ZonedDateTime test = (Instant.FromDateTimeOffset(DateTime.Now)).InZone(tz);
+            txtDate.Text = now.InUtc().ToString();
+            txtDateTimeNow.Text = test.ToString();
+            //comboBox1.DataSource = TzdbDateTimeZoneSource.Default
+            //    .ZoneLocations.OrderBy(t => t.ZoneId)
+            //    .ToList();
+            //comboBox1.DisplayMember = "ZoneId";
+            var zonesss = TzdbDateTimeZoneSource.Default.Aliases.ToList();
+            comboBox1.DataSource = TzdbDateTimeZoneSource.Default.ZoneLocations.OrderBy(o=>o.ZoneId).ToList();
+            comboBox1.DisplayMember = "ZoneId";
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures & ~CultureTypes.SpecificCultures);
+            comboBox1.DataSource = cultures.ToList();
+            comboBox1.DisplayMember = "DisplayName";
+            comboBox1.SelectedItem = defaultCulture;
+
+            
+
+            txtInstant.Text = "Instant: " + now.ToString() + Environment.NewLine;
+            txtInstant.Text += "ZonedDateTime: " + now.InUtc().ToString() + Environment.NewLine;
+            txtInstant.Text += "Time Zone: " + tz.Id.ToString() + Environment.NewLine;
+            txtInstant.Text += "Local Time: " + now.InZone(tz).ToString() + Environment.NewLine;
+            txtInstant.Text += "Offset: " + tz.MaxOffset.ToString() + Environment.NewLine;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTimeZone tz = DateTimeZoneProviders.Tzdb["Europe/London"];
+            var appointment = LocalDateTime.FromDateTime(DateTime.Now);
+            
+            MessageBox.Show(appointment.ToString());
+            MessageBox.Show(appointment.InZoneStrictly(tz).ToString());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //DateTimeZone tz = DateTimeZoneProviders.Tzdb[time_zone.StandardName];
+            if (comboBox1.SelectedItem is CultureInfo culture)
+            {
+                //store the LCID in the database
+                culture = new CultureInfo(culture.Name);
+                textBox1.Text = culture.DisplayName;
+                textBox1.Text += Environment.NewLine + culture.Name;
+                textBox1.Text += Environment.NewLine + DateTime.Now.ToString(culture);
+                textBox1.Text += Environment.NewLine + String.Format(culture, "{0:C}", 10.545);
+                ///-----
+                // This gets an instant from stored time
+                LocalDateTime localDateTime = LocalDateTime.FromDateTime(dateTimePicker1.Value);
+                var timeZone = DateTimeZoneProviders.Tzdb["Europe/London"];
+                var zonedDateTime = localDateTime.InZoneLeniently(timeZone);
+                //quicker way of doing it ---VVV--
+                timeZone.AtLeniently(localDateTime).ToInstant();
+                var instant = zonedDateTime.ToInstant();
+                textBox1.Text += Environment.NewLine + "---------------------------------------";
+                textBox1.Text += Environment.NewLine + "Local Date Time: " + localDateTime.ToString();
+                textBox1.Text += Environment.NewLine + "Zoned Date Time: " + zonedDateTime.ToString();
+                textBox1.Text += Environment.NewLine + "Instant: " + instant.ToString();
+            }
+        }
+    }
+}
